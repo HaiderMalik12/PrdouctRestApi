a95da56952f3356de8e6a9ad428d9b194076e5ff
Haider Malik <haidermalik5044@gmail.com>
2016-01-21 21:51:39 -0800

    Add GET:/products

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var express=require('express');
 var app=express();
 var PORT=process.env.PORT || 3000;
+var products=[{
+	id:1,
+	name:'Krizma Lawn',
+	description:'This Branded Suit',
+	price:23.0,
+	brandName:'Krizma',
+	type:'Vol 3'
+},
+
+{
+id:2,
+	name:'Maria B Lawn',
+	description:'This is Maria b Replica',
+	price:23.0,
+	brandName:'Maria B',
+	type:'Vol 2'
+}];
 
 app.get('/',function(req,res){
  
@@ -8,6 +25,12 @@ app.get('/',function(req,res){
  
 });
 
+
+// GET:/products
+
+app.get('/products',function(req,res){
+res.json(products);
+});
 app.listen(PORT,function(){
 console.log('Express listening on PORT: '+PORT +' !' );
 });
